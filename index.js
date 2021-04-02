65061a822ab69ccdb82453f311e367625bbf0af3
nrd141913 <nrd141913@gmail.com>
2021-04-02 23:31:13 +0600

    heroku restore

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World !!!')
+  res.send('Hello World !')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghclx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
